c2611009887019798c3c282c1e10bddbcd6a909d
Matthew O'Brien <matthew.obrien@fox.com>
2018-01-10 09:42:37 -0800

    initial commit

---
+++ ccae-streams-app/src/main/java/ccae/streams/CCAEStreams.java
@@ -0,0 +1,109 @@
+package ccae.streams;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import io.confluent.connect.avro.CheckedInOutRights;
+import io.confluent.connect.avro.ContractTitleList;
+import io.confluent.connect.avro.DealInfo;
+import io.confluent.connect.avro.LicenseRight;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+
+public class CCAEStreams 
+{
+	static final Properties streamsConfig = new Properties();
+	static final KStreamBuilder builder = new KStreamBuilder();
+	
+	// Contract Title List
+	static final String INPUT_TOPIC_CONTRACT = "tuscany-contract-title-list";
+	static final String OUTPUT_TOPIC_CONTRACT = "ccae-deal-info";
+	static final SpecificAvroSerde<ContractTitleList> sourceSerdeContract = new SpecificAvroSerde<>();
+	static final SpecificAvroSerde<DealInfo> sinkSerdeContract = new SpecificAvroSerde<>();
+	
+	// License Rights
+	static final String INPUT_TOPIC_RIGHTS = "tuscany-license-right";
+	static final String OUTPUT_TOPIC_RIGHTS = "ccae-checked-in-out-rights";
+	static final SpecificAvroSerde<LicenseRight> sourceSerdeRights = new SpecificAvroSerde<>();
+	static final SpecificAvroSerde<CheckedInOutRights> sinkSerdeRights = new SpecificAvroSerde<>();
+
+	public static void main(String[] args) {
+		final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
+		final String schemaRegistryUrl = args.length > 1 ? args[1] : "http://localhost:8081";
+
+		KafkaStreams streams = createStreams(bootstrapServers, schemaRegistryUrl);
+		streams.cleanUp();
+		streams.start();
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> streams.close()));
+	}
+
+	public static void configureStreams(String bootstrapServers, String schemaRegistryUrl) {
+		streamsConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "deal-info-streams");
+		streamsConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		streamsConfig.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+		streamsConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+		sourceSerdeContract.configure(serdeConfig, false);
+		sinkSerdeContract.configure(serdeConfig, false);
+	}
+
+	public static KStreamBuilder buildStream() {
+		KStream<String, ContractTitleList> dealInfoStreamIn = builder.stream(Serdes.String(), sourceSerdeContract, INPUT_TOPIC_CONTRACT);
+
+		KStream<String, DealInfo> dealInfoStreamOut = dealInfoStreamIn.map((k,
+				v) -> new KeyValue<>(v.getCONTRACTID().toString(), 
+						new DealInfo(
+						v.getBILLINGUNITCODE(), 	// BUSINESS_UNIT_CODE
+						v.getCURRENCYID(), 			// CURRENCY_ID
+						v.getCUSTOMERID(), 			// CUSTOMER_ID
+						v.getCONTRACTID(), 			// DEAL_ID
+						v.getCONTRACTSTATUSID(),	// DEAL_STATUS_ID
+						v.getCONTRACTTYPEID(), 		// DEAL_TYPE_ID
+						v.getTITLELISTENDDATE(),	// MAX_END_DATE
+						v.getTITLELISTSTARTDATE(),	// MIN_START_DATE
+						v.getTITLELISTDESCRIPTION()	// NAME
+		)));
+		dealInfoStreamOut.to(Serdes.String(), sinkSerdeContract, OUTPUT_TOPIC_CONTRACT);
+		
+		KStream<String, LicenseRight> checkedInOutRightsStreamIn = builder.stream(Serdes.String(), sourceSerdeRights, INPUT_TOPIC_RIGHTS);
+		
+		KStream<String, CheckedInOutRights> checkedInOutRightsStreamOut = checkedInOutRightsStreamIn.map((k,
+				v) -> new KeyValue<>(v.getLICENSERIGHTSID().toString(),
+						new CheckedInOutRights(null, 		// missing CURRENCY_ID (CurrencyId)
+								null, 						// missing CUSTOMER_ID (CustomerId)
+								v.getTLRENDDATE(),			// END_DATE
+								v.getLICENSERIGHTSID(), 	// ID
+								v.getLANGUAGEID(), 			// LANGUAGE_ID
+								null, 						// missing LICENSE_FEE (TotalFees)
+								v.getMEDIAID(),				// MEDIA_ID
+								null, 						// missing PRODUCT_ID (ProductId)
+								v.getTITLELICENSERIGHTID(), // RIGHTS_GROUP_ID
+								v.getLICENSETYPEID(), 		// RIGHT_TYPE_ID
+								v.getMLTGROUPID(), 			// SOURCE_DETAIL_ID
+								null, 						// missing SOURCE_ID (TitleListId)
+								v.getTLRSTARTDATE(), 		// START_DATE
+								v.getTERRITORYID() 			// TERRITORY_ID
+						)
+		));
+		checkedInOutRightsStreamOut.to(Serdes.String(), sinkSerdeRights, OUTPUT_TOPIC_RIGHTS);
+		
+		return builder;
+	}
+
+	public static KafkaStreams createStreams(String bootstrapServers, String schemaRegistryUrl) {
+		configureStreams(bootstrapServers, schemaRegistryUrl);
+		buildStream();
+		return new KafkaStreams(builder, new StreamsConfig(streamsConfig));
+	}
+
+}
---
+++ ccae-streams-app/src/test/java/ccae/streams/CCAEStreamsTest.java
@@ -0,0 +1,153 @@
+package ccae.streams;
+
+import java.util.GregorianCalendar;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.test.ProcessorTopologyTestDriver;
+
+import io.confluent.connect.avro.CheckedInOutRights;
+import io.confluent.connect.avro.ContractTitleList;
+import io.confluent.connect.avro.DealInfo;
+import io.confluent.connect.avro.LicenseRight;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Simple unit test.
+ */
+public class CCAEStreamsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CCAEStreamsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CCAEStreamsTest.class );
+    }
+
+    public void testApp()
+    {
+    	// Set up mock and expected records 
+    	// Contract
+		ContractTitleList recInContract = new ContractTitleList();
+		recInContract.setBILLINGUNITCODE("Tel");
+		recInContract.setCURRENCYID(20);
+		recInContract.setCUSTOMERID(20L);
+		recInContract.setCONTRACTID(123456L);
+		recInContract.setCONTRACTSTATUSID(1);
+		recInContract.setCONTRACTTYPEID(1);
+		recInContract.setTITLELISTENDDATE(new GregorianCalendar(2017, 10, 10).getTimeInMillis());
+		recInContract.setTITLELISTSTARTDATE(new GregorianCalendar(2015, 10, 10).getTimeInMillis());
+		recInContract.setTITLELISTDESCRIPTION("TEST");
+		
+		DealInfo expContract = new DealInfo();
+		expContract.setBUSINESSUNITCODE("Tel");
+		expContract.setCURRENCYID(20);
+		expContract.setCUSTOMERID(20L);
+		expContract.setDEALID(123456L);
+		expContract.setDEALSTATUSID(1);
+		expContract.setDEALTYPEID(1);
+		expContract.setMAXENDDATE(new GregorianCalendar(2017, 10, 10).getTimeInMillis());
+		expContract.setMINSTARTDATE(new GregorianCalendar(2015, 10, 10).getTimeInMillis());
+		expContract.setNAME("TEST");
+		
+		// Rights
+		LicenseRight recInRights = new LicenseRight();
+		recInRights.setAPPLICATIONID(810);
+		recInRights.setCARVEOUTCUSTOMERLIMIT(3);
+		recInRights.setCARVEOUTDETAILS("2888,1243");
+		recInRights.setCATCHUPDURATIONLIMIT(25);
+		recInRights.setCATCHUPEPISODELIMIT(10);
+		recInRights.setGROUPID(1111L);
+		recInRights.setHASHVALUE(12);
+		recInRights.setLANGUAGEEXCLUSIONID(2222L);
+		recInRights.setLANGUAGEID(3333L);
+		recInRights.setLICENSERIGHTSID(4444L);
+		recInRights.setLICENSESUBTYPEID(12);
+		recInRights.setLICENSETYPEID(1);
+		recInRights.setMEDIAID(5555L);
+		recInRights.setMLTGROUPID(6666L);
+		recInRights.setMONTHS(1);
+		recInRights.setRIGHTSENDDATETBD("9999-09-09T07:00:00.000+0000");
+		recInRights.setRIGHTSSTARTDATETBD("9999-09-09T07:00:00.000+0000");
+		recInRights.setTENTATIVE(1);
+		recInRights.setTERRITORYID(7777L);
+		recInRights.setTITLELICENSERIGHTID(8888L);
+		recInRights.setTITLELISTMAPID(9999L);
+		recInRights.setTLRENDDATE("9/30/2017");
+		recInRights.setTLRSTARTDATE("9/7/2017");
+		
+		CheckedInOutRights expRights = new CheckedInOutRights();
+		expRights.setCURRENCYID(19);
+		expRights.setCUSTOMERID(409L);
+		expRights.setENDDATE("7/1/2019");
+		expRights.setID(1111L);
+		expRights.setLANGUAGEID(2L);
+		expRights.setLICENSEFEE(0);
+		expRights.setMEDIAID(2222L);
+		expRights.setPRODUCTID(3333L);
+		expRights.setRIGHTSGROUPID(4444L);
+		expRights.setRIGHTTYPEID(21);
+		expRights.setSOURCEDETAILID(5555L);
+		expRights.setSOURCEID(6666L);
+		expRights.setSTARTDATE("1/1/2017");
+		expRights.setTERRITORYID(7777L);
+		
+		
+		// Configure and build streams app
+		CCAEStreams.configureStreams("localhost:9092", "http://localhost:8081");
+		CCAEStreams.buildStream();
+		
+		// Pipe record(s) through streams app
+		ProcessorTopologyTestDriver driver = new ProcessorTopologyTestDriver(new StreamsConfig(CCAEStreams.streamsConfig), CCAEStreams.builder);
+		// Contract
+		driver.process(CCAEStreams.INPUT_TOPIC_CONTRACT, "1", recInContract, Serdes.String().serializer(), CCAEStreams.sourceSerdeContract.serializer());
+		ProducerRecord<String, DealInfo> recOutContract = driver.readOutput(CCAEStreams.OUTPUT_TOPIC_CONTRACT, Serdes.String().deserializer(), CCAEStreams.sinkSerdeContract.deserializer());
+		// Rights
+		driver.process(CCAEStreams.INPUT_TOPIC_RIGHTS, "1", recInRights, Serdes.String().serializer(), CCAEStreams.sourceSerdeRights.serializer());
+		ProducerRecord<String, CheckedInOutRights> recOutRights = driver.readOutput(CCAEStreams.OUTPUT_TOPIC_RIGHTS, Serdes.String().deserializer(), CCAEStreams.sinkSerdeRights.deserializer());
+		
+		
+		// Validate output record(s)
+		// Contract
+		assertEquals(expContract.getBUSINESSUNITCODE().toString(), recOutContract.value().getBUSINESSUNITCODE().toString());
+		assertEquals(expContract.getCURRENCYID(), recOutContract.value().getCURRENCYID());
+		assertEquals(expContract.getCUSTOMERID(), recOutContract.value().getCUSTOMERID());
+		assertEquals(expContract.getDEALID(), recOutContract.value().getDEALID());
+		assertEquals(expContract.getDEALSTATUSID(), recOutContract.value().getDEALSTATUSID());
+		assertEquals(expContract.getDEALTYPEID(), recOutContract.value().getDEALTYPEID());
+		assertEquals(expContract.getMAXENDDATE(), recOutContract.value().getMAXENDDATE());
+		assertEquals(expContract.getMINSTARTDATE(), recOutContract.value().getMINSTARTDATE());
+		assertEquals(expContract.getNAME().toString(), recOutContract.value().getNAME().toString());
+		
+		// Rights
+		assertEquals(expRights.getCURRENCYID(), recOutRights.value().getCURRENCYID());
+		assertEquals(expRights.getCUSTOMERID(), recOutRights.value().getCUSTOMERID());
+		assertEquals(expRights.getENDDATE().toString(), recOutRights.value().getENDDATE().toString());
+		assertEquals(expRights.getID(), recOutRights.value().getID());
+		assertEquals(expRights.getLANGUAGEID(), recOutRights.value().getLANGUAGEID());
+		assertEquals(expRights.getLICENSEFEE(), recOutRights.value().getLICENSEFEE());
+		assertEquals(expRights.getMEDIAID(), recOutRights.value().getMEDIAID());
+		assertEquals(expRights.getPRODUCTID(), recOutRights.value().getPRODUCTID());
+		assertEquals(expRights.getRIGHTSGROUPID(), recOutRights.value().getRIGHTSGROUPID());
+		assertEquals(expRights.getRIGHTTYPEID(), recOutRights.value().getRIGHTTYPEID());
+		assertEquals(expRights.getSOURCEDETAILID(), recOutRights.value().getSOURCEDETAILID());
+		assertEquals(expRights.getSOURCEID(), recOutRights.value().getSOURCEID());
+		assertEquals(expRights.getSTARTDATE().toString(), recOutRights.value().getSTARTDATE().toString());
+		assertEquals(expRights.getTERRITORYID(), recOutRights.value().getTERRITORYID());
+    }
+}
